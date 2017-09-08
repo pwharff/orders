20dc957e2743071271406b91aff5a23c03e5cd49
Unknown <alfredo.velicaria@safecu.org>
2017-09-08 14:53:20 -0700

    Not quite working

--- server.js
+++ server.js
@@ -2,23 +2,43 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
+const _ = require('lodash')
+
 const db = {
     orders: {
-        1 : "Pizza",
-        2 : "Salad",
-        3 : "Donuts"
+        1: "Pizza",
+        2: "Salad",
+        3: "Donuts"
     }
 }
 
 app.use(bodyParser.json())
 
-app.use((req,res,next)=>{
-    req.mike = true
-    next()
-})
+const getOkSoon = sec =>
+    new Promise((resolve, reject) =>
+        setTimeout(() => resolve({ok: 'all good!'}), sec * 1000))
 
-app.post('/orders', (req, res)=> {
-    res.send(db.orders)
+app.post('/orders', async (req, res) => {
+    const id = _.maxBy(Object.keys(db.orders), key => Number(key)) + 1
+    const {type} = req.body
+    //some kind of validation of type here -- probably automatic using joi
+    const newOrder = {
+        id,
+        type
+    }
+    db.orders[id] = newOrder
+    //hit the other API to place the shipment!!!
+    // const shipment = await fetch('https://api.github.com/users/defunkt')
+    try {
+        const shipment = await fetch('https://er-shipping.herokuapp.com/shipments', {method: 'get'})
+    } catch (excp) {
+        console.log (excp)
+    }
+    res.send({
+        ok: true,
+        shipment,
+        value: newOrder
+    })
 })
 
 app.get('/', function (req, res) {
